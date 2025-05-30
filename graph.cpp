bc9b57a956571c6eaa80e1559636a7e3e0efcd17
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-05-29 21:09:37 -0700

    more fixes

--- graph.cpp
+++ graph.cpp
@@ -131,7 +131,7 @@ void graph::addEdge(const char* from, const char* to, int weight) {
     current = current->next;
   }
 
-  edge* newEdge = new edge(tIndex, weight, nodes[fIndex]->head);
+  edge* newEdge = new edge{tIndex, weight, nodes[fIndex]->head};
   nodes[fIndex]->head = newEdge;
 }
 
