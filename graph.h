c39562c898d0b99e231edc5a0a7c56039466d429
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-05-29 17:52:22 -0700

    graph class header

--- graph.h
+++ graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include <vector>
+using namespace std;
 
 struct edge {
   int toIndex;
@@ -16,3 +17,23 @@ struct node {
   node(const char* label);
   ~node();
 };
+
+class graph {
+private:
+  vector<node*> nodes;
+  int getIndex(const char* label);
+
+public:
+  void addVertex(const char* label);
+  void removeVertex(const char* label);
+
+  void addEdge(const char* from, const char* to, int weight);
+  void removeEdge(const char* from, const char* to);
+
+  void printAdjacencyList();
+   void shortestPath(const char* from, const char* to);
+    ~graph();
+
+};
+
+#endif
